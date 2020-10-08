f86aa976347828dde813f105c4256ffd4222dac1
chuck francis <chuck_francis10@yahoo.com>
2020-10-08 12:01:39 -0400

    Add project files.

---
+++ joe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace joe
+{
+    class Person
+    {
+        Person Joe = new Person();
+        public string name;
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine($"Hi, I'm {name}");
+        }
+    }
+}
